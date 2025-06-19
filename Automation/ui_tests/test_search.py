764210ac171a6a706ffac13f45717c12fbde167f
YuVo77 <hardcore.metal78@gmail.com>
2025-06-19 18:39:33 +0300

    Update code

--- Automation/ui_tests/test_search.py
+++ Automation/ui_tests/test_search.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as expected_conditions
 
 
 def test_search(browser):
@@ -11,7 +13,12 @@ def test_search(browser):
     search_button = browser.find_element(By.CSS_SELECTOR, 'button.action.search')
     search_button.click()
 
+    WebDriverWait(browser, 10).until(
+        expected_conditions.presence_of_element_located((By.CSS_SELECTOR, 'a.product-item-link'))
+    )
+
     product_titles = browser.find_elements(By.CSS_SELECTOR, 'a.product-item-link')
     titles_text = [title.text.lower() for title in product_titles]
 
     assert any(query in title for title in titles_text), "No products found with the search query"
+
