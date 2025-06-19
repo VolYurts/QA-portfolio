f8541acba72b0ad6192a53570fb784f9f11698ce
YuVo77 <hardcore.metal78@gmail.com>
2025-06-19 20:42:18 +0300

    Add workflow

--- Automation/ui_tests/conftest.py
+++ Automation/ui_tests/conftest.py
@@ -1,9 +1,16 @@
 import pytest
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture
 def browser():
-    driver = webdriver.Chrome()
+    options = Options()
+    options.add_argument('--headless')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--disable-dev-shm-usage')
+    options.add_argument('--window-size=1920,1080')
+
+    driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
     yield driver
     driver.quit()
