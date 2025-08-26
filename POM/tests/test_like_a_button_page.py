52fafca5459abdc30e2a211803df0067738e8b1a
VolodymyrYurtsan <hardcore.metal78@gmail.com>
2025-08-26 14:27:06 +0300

    Update test

--- POM/tests/test_like_a_button_page.py
+++ POM/tests/test_like_a_button_page.py
@@ -1,11 +1,13 @@
+from selenium.webdriver.remote.webdriver import WebDriver
 from POM.pages.like_a_button import LikeAButton
 
-def test_button2_exist(browser):
+
+def test_button2_exist(browser: WebDriver) -> None:
     like_a_button = LikeAButton(browser)
     like_a_button.open()
     assert like_a_button.button_is_displayed
 
-def test_button2_clicked(browser):
+def test_button2_clicked(browser: WebDriver) -> None:
     like_a_button = LikeAButton(browser)
     like_a_button.open()
     like_a_button.button_click()
