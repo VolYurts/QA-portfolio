da01c6599770ed7a0ab29cd66b81fca642b6d68a
VolodymyrYurtsan <hardcore.metal78@gmail.com>
2025-08-26 14:27:32 +0300

    Update test

--- POM/tests/test_simple_button_page.py
+++ POM/tests/test_simple_button_page.py
@@ -1,12 +1,13 @@
+from selenium.webdriver.remote.webdriver import WebDriver
 from POM.pages.simple_button import SimpleButtonPage
 
 
-def test_button1_exist(browser):
+def test_button1_exist(browser: WebDriver) -> None:
     simple_page = SimpleButtonPage(browser)
     simple_page.open()
     assert simple_page.button_is_displayed()
 
-def test_button1_clicked(browser):
+def test_button1_clicked(browser: WebDriver) -> None:
     simple_page = SimpleButtonPage(browser)
     simple_page.open()
     simple_page.click_button()
