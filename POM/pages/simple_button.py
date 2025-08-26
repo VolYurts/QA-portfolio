53718b3f99af1ffb651cb6ab749a2926320a3dfc
VolodymyrYurtsan <hardcore.metal78@gmail.com>
2025-08-26 14:26:20 +0300

    Update code

--- POM/pages/simple_button.py
+++ POM/pages/simple_button.py
@@ -1,31 +1,35 @@
-from POM.pages.base_page import BasePage
+from typing import Tuple
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 from selenium.webdriver.common.by import By
 
+from POM.pages.base_page import BasePage
 
-button_selector = (By.ID, 'submit-id-submit')
-result_selector = (By.ID, 'result-text')
+
+button_selector: Tuple[str, str] = (By.ID, 'submit-id-submit')
+result_selector: Tuple[str, str] = (By.ID, 'result-text')
 
 
 class SimpleButtonPage(BasePage):
-    def __init__(self, browser):
+    def __init__(self, browser: WebDriver) -> None:
         super().__init__(browser)
 
-    def open(self):
+    def open(self) -> None:
         self.browser.get('https://www.qa-practice.com/elements/button/simple')
 
-    def button(self):
+    def button(self) -> WebElement:
         return self.find(button_selector)
 
-    def button_is_displayed(self):
+    def button_is_displayed(self) -> bool:
         return self.button().is_displayed()
 
-    def click_button(self):
+    def click_button(self) -> None:
         self.button().click()
 
-    def result(self):
+    def result(self) -> WebElement:
         return self.find(result_selector)
 
     @property
-    def result_text(self):
+    def result_text(self) -> str:
         return self.result().text
 
