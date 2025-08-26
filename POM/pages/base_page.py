2b967fae89dd185ff9667e134a62530d94debd5d
VolodymyrYurtsan <hardcore.metal78@gmail.com>
2025-08-26 14:25:05 +0300

    Update code

--- POM/pages/base_page.py
+++ POM/pages/base_page.py
@@ -1,6 +1,12 @@
-class BasePage:
-    def __init__(self, browser):
-        self.browser = browser
+from typing import Tuple
+from selenium.webdriver.remote.webdriver import WebDriver
+from selenium.webdriver.remote.webelement import WebElement
 
-    def find(self, args):
+
+class BasePage:
+    def __init__(self, browser: WebDriver) -> None:
+        self.browser: WebDriver = browser
+
+    def find(self, args: Tuple[str, str]) -> WebElement:
         return self.browser.find_element(*args)
+        
